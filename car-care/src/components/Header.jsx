aa9d7ec3871ac4ae3511e6310641e598959eb8aa
Laith <laythalramhi@gmail.com>
2025-04-24 20:58:10 +0400

    NEW admin

---
+++ car-care/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>Header</div>
+  )
+}
+
+
+// Top bar with:
+
+// Admin name/profile
+
+// Logout button
+
+// Notification bell (optional)
